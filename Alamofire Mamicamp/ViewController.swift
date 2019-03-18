241b6db304a1dfee7d9fff3e2378330769d7815d
Saiful <icungse@gmail.com>
2019-03-19 00:50:44 +0700

    Initial Commit

---
+++ Alamofire Mamicamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alamofire Mamicamp
+//
+//  Created by icungse on 19/03/19.
+//  Copyright © 2019 icung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
